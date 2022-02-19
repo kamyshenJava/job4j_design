78c7b2fc6bdcebddcb4c266870e236df2e44a765
kamyshenJava <kamyshen@tutanota.com>
2022-02-19 19:31:11 +0300

    5. Очередь на двух стеках [#160 #266349]. Добавил исключение при пустых стэках.

--- src/main/java/ru/job4j/collection/list/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/list/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection.list;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -7,6 +9,9 @@ public class SimpleQueue<T> {
     private int countOut;
 
     public T poll() {
+        if (countOut == 0 && countIn == 0) {
+            throw new NoSuchElementException();
+        }
         if (countOut == 0) {
             while (countIn != 0) {
                 out.push(in.pop());
