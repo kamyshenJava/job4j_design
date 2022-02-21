e70f40a45f49af0b6b9c25bcbe544568b6c7c869
kamyshenJava <kamyshen@tutanota.com>
2022-02-21 10:29:36 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+
+public class SimpleTreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertFalse(tree.add(2, 6));
+    }
+
+    @Test
+    public void whenAddElementsThenListOfChildren() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        assertThat(2,
+                is(tree.findBy(1).get().children.get(0).value));
+        assertThat(3,
+                is(tree.findBy(1).get().children.get(1).value));
+        assertThat(4,
+                is(tree.findBy(1).get().children.get(2).value));
+        assertThat(5,
+                is(tree.findBy(4).get().children.get(0).value));
+        assertThat(6,
+                is(tree.findBy(5).get().children.get(0).value));
+    }
+}
