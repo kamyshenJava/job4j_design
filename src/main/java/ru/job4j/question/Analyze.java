31781360f7ded9b27df713b262ac814b17f3d3b8
kamyshenJava <kamyshen@tutanota.com>
2022-03-13 16:20:09 +0300

    2. Статистика по коллекции. [#45889 #274577]

--- src/main/java/ru/job4j/question/Analyze.java
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -2,26 +2,29 @@ package ru.job4j.question;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Analyze {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info rsl = new Info(0, 0, 0);
-        int changed = 0;
-        int added = current.size();
-        for (User cur : current) {
-            for (User prev : previous) {
-                if (cur.getId() == prev.getId() && !cur.getName().equals(prev.getName())) {
-                    changed++;
-                }
-                if (cur.equals(prev)) {
-                    added--;
-                }
-            }
-        }
+        Set<User> copyCurrent = new HashSet<>(current);
+        copyCurrent.removeAll(previous);
+        Set<Integer> prevIds = getSetOfIds(previous);
+        Set<Integer> curIds = getSetOfIds(current);
+        curIds.removeAll(prevIds);
+        int added = curIds.size();
+        int deleted = previous.size() - current.size() + added;
+        int changed = copyCurrent.size() - added;
         rsl.setChanged(changed);
-        rsl.setAdded(added - changed);
-        rsl.setDeleted(previous.size() - current.size() + added - changed);
+        rsl.setAdded(added);
+        rsl.setDeleted(deleted);
         return rsl;
+    }
+
+    private static Set<Integer> getSetOfIds(Set<User> set) {
+        return set.stream()
+                .map(User::getId)
+                .collect(Collectors.toSet());
     }
 }
