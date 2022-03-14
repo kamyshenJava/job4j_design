9a743e965ba75fadff71e4f21dec5e6ccb1745f7
kamyshenJava <kamyshen@tutanota.com>
2022-03-14 08:35:21 +0300

    0.3. BufferedReader. [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -27,7 +27,6 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        log.stream()
-                .forEach(System.out::println);
+        log.forEach(System.out::println);
     }
 }
