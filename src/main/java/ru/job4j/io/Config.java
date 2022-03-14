95e128bc2ab3ae5609278a4e632dffd6109e9c0b
kamyshenJava <kamyshen@tutanota.com>
2022-03-14 14:53:58 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines()
+                    .filter(x -> !x.equals(""))
+                    .filter(x -> !x.startsWith("#"))
+                    .filter(this::checkThePattern)
+                    .map(x -> x.split("="))
+                    .forEach(x -> values.put(x[0], x[1]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkThePattern(String str) throws IllegalArgumentException {
+        String[] tmp = str.split("=");
+        boolean rsl = tmp.length == 2 && !tmp[0].isEmpty() && !tmp[1].isEmpty();
+        if (!rsl) {
+            throw new IllegalArgumentException();
+        }
+       return true;
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(nullValue()));
+    }
+
+    @Test
+    public void whenPairWithCommentAndEmptyString() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr"));
+        assertThat(config.value("surname"), is("Arsentev"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairWithBrokenLines() {
+        String path = "./data/pair_with_broken_lines.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+
+}
