a6dee57eecc45c4092eebe36088aad28f3aaf1c7
kamyshenJava <kamyshen@tutanota.com>
2022-03-15 10:25:16 +0300

    2. Анализ доступности сервера. [#859 #275353]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -11,15 +11,13 @@ public class Analizy {
         StringJoiner sj = new StringJoiner(";", "", ";");
         try (BufferedReader in = new BufferedReader(new FileReader(source)); PrintWriter out =
                 new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            List<String[]> lines = in.lines()
-                    .map(x -> x.split(" "))
-                    .collect(Collectors.toList());
-        for (String[] line : lines) {
-            if (!idle && (line[0].equals("400") || line[0].equals("500"))) {
+        for (String l = in.readLine(); l != null; l = in.readLine()) {
+            String[] line = l.split(" ");
+            if (!idle && ("400".equals(line[0]) || "500".equals(line[0]))) {
                 sj.add(line[1]);
                 idle = true;
             }
-            if (idle && (line[0].equals("200") || line[0].equals("300"))) {
+            if (idle && ("200".equals(line[0]) || "300".equals(line[0]))) {
                 sj.add(line[1]);
                 out.println(sj);
                 sj = new StringJoiner(";", "", ";");
@@ -32,12 +30,6 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         Analizy analizy = new Analizy();
         analizy.unavailable("idleLog.txt", "idleLogFiltered.txt");
     }
