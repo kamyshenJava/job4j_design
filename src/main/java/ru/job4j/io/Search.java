0ec70917a803f73d084270b2a397ae44c4077e26
kamyshenJava <kamyshen@tutanota.com>
2022-03-16 08:26:44 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".class")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
