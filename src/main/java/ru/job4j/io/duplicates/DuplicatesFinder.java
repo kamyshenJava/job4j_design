176a0a9e204b48a381f6c939dadc178aa911f14d
kamyshenJava <kamyshen@tutanota.com>
2022-03-16 11:01:23 +0300

    4.2. Поиск дубликатов [#315066 #275898]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
+
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("C:\\projects"), duplicatesVisitor);
         for (List<Path> value : duplicatesVisitor.getFilePaths().values()) {
