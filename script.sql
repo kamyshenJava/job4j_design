8f1c6a1e1f2b965d3a218954466e41b80af0e74f
kamyshenJava <kamyshen@tutanota.com>
2022-03-27 02:06:08 +0300

    0. Что такое база данных. [#6860 #280484]

--- script.sql
+++ script.sql
@@ -1,3 +1,10 @@
+create table computers(
+    id serial primary key,
+    name varchar(255),
+    manufacturer text,
+    isLaptop boolean
+);
+
 insert into computers(name, manufacturer, isLaptop) values('Lenovo IdeaPad', 'Asus', true);
 select * from computers;
 
