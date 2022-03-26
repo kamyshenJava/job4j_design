9754578eed808a1293b77399ca0ff5209a20a681
kamyshenJava <kamyshen@tutanota.com>
2022-03-26 04:08:08 +0300

    0. Что такое база данных. [#6860]

---
+++ script.sql
@@ -0,0 +1,7 @@
+insert into computers(name, manufacturer, isLaptop) values('Lenovo IdeaPad', 'Asus', true);
+select * from computers;
+
+update computers set isLaptop = false;
+select * from computers;
+
+delete from computers;
