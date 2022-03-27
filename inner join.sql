00317a289085c27eb3ce67c219b5d25c62d373ce
kamyshenJava <kamyshen@tutanota.com>
2022-03-27 13:14:42 +0300

    1. Inner join [#373617 #280830]

--- inner join.sql
+++ inner join.sql
@@ -11,13 +11,26 @@ create table books(
 	author_id int references authors(id)
 );
 
-insert into authors(name, birth, death) values('Pushkin', '1799-01-01', '1837-01-29');
-insert into authors(name, birth, death) values('Lermontov', '1814-10-15', '1841-07-27');
-insert into books(name, date_of_writting, author_id) values('Eugene Onegin', 1832, 1);
-insert into books(name, date_of_writting, author_id) values('Ruslan and Lyudmila', 1820, 1);
-insert into books(name, date_of_writting, author_id) values('A Hero of Our Time', 1840, 2);
-insert into books(name, date_of_writting, author_id) values('Borodino', 1837, 2);
+insert into authors(name, birth, death)
+values('Pushkin', '1799-01-01', '1837-01-29');
+insert into authors(name, birth, death)
+values('Lermontov', '1814-10-15', '1841-07-27');
+insert into books(name, date_of_writting, author_id)
+values('Eugene Onegin', 1832, 1);
+insert into books(name, date_of_writting, author_id)
+values('Ruslan and Lyudmila', 1820, 1);
+insert into books(name, date_of_writting, author_id)
+values('A Hero of Our Time', 1840, 2);
+insert into books(name, date_of_writting, author_id)
+values('Borodino', 1837, 2);
 
-select a.name as Имя, b.name as Книга from authors as a join books as b on a.id = b.author_id;
-select a.name, b.name, b.date_of_writting as дата_написания from authors as a join books as b on a.id = b.author_id;
-select a.name, a.birth, a.death, b.name, b.date_of_writting from authors as a join books as b on a.id = b.author_id;
+select a.name as Имя, b.name as Книга
+from authors as a join books as b on a.id = b.author_id;
+select a.name, b.name, b.date_of_writting as дата_написания
+from authors as a
+join books as b
+on a.id = b.author_id;
+select a.name, a.birth, a.death, b.name, b.date_of_writting
+from authors as a
+join books as b
+on a.id = b.author_id;
