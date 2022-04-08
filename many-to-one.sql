e1f2fcf6d1c1ba482425ddd3383101a35849c15e
kamyshenJava <kamyshen@tutanota.com>
2022-04-08 06:58:49 +0300

    0. Понятие сборки мусора [#6851]

--- many-to-one.sql
+++
@@ -1,9 +0,0 @@
-create table authors(
-    id serial primary key, 
-    name varchar(255),
-);
-create table books(
-    id serial primary key, 
-    name varchar(255),
-	author_id int references authors(id)
-);
--- select_from_fauna.sql
+++
@@ -1,4 +0,0 @@
-select * from fauna where name like '%fish%';
-select * from fauna where 10000 < avg_age and avg_age < 21000;
-select * from fauna where discovery_date is null;
-select * from fauna where discovery_date < '01.01.1950';
