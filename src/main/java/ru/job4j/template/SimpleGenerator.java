15e05718896946c2d0e0766ec9ec0eda841cf41b
kamyshenJava <kamyshen@tutanota.com>
2022-04-16 14:09:35 +0300

    3. Шаблонизатор. [#855]

---
+++ src/main/java/ru/job4j/template/SimpleGenerator.java
@@ -0,0 +1,9 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+public class SimpleGenerator implements Generator {
+    public String produce(String template, Map<String, String> args) {
+        return "";
+    }
+}
---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenGenerate() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        Generator gen = new SimpleGenerator();
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        assertThat(gen.produce(template, args), is(expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotAllKeysThenException() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        Generator gen = new SimpleGenerator();
+        gen.produce(template, args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExcessKeysThenException() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("key", "value");
+        Generator gen = new SimpleGenerator();
+        gen.produce(template, args);
+    }
+}
